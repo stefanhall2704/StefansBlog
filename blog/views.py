9e968a94d51db56b86403ea798bb1055dd41e9e4
Stefan Hall <stefhall.2704@gmail.com>
2021-07-25 21:53:40 -0400

    Layout for button to Post New

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,11 @@ from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,6 +26,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
